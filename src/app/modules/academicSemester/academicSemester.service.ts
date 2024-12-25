7e2a47ba7141d9030310a02e479b5fe94910db56
hexvoncode <hexvontech@gmail.com>
2024-12-25 23:14:01 +0600

    AppError

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,11 +1,13 @@
 import { TAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 import { academicSemesterNameCodeMapper } from './academicSemester.constants';
+import AppError from '../../errors/AppError';
+import { StatusCodes } from 'http-status-codes';
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
 
   if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
-    throw new Error('Invalid Semester code');
+    throw new AppError(StatusCodes.BAD_REQUEST,'Invalid Semester code');
   }
   const result = await AcademicSemester.create(payload);
   return result;
